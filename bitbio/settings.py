bdb6eef30fe266b0a70f30d5840aafd65d97a3aa
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-08-28 12:49:13 +0500

    settings updated with BASE_DIR

--- bitbio/settings.py
+++ bitbio/settings.py
@@ -142,7 +142,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "bitbio" / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "bitbio" / "static",
+    BASE_DIR / "calculator" / "static",
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type

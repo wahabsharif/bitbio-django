cc2f71e30eb2cdd8821c387cf68a81d2d431cf9c
Muhammad Wahab Sharif <www.rex19@gmail.com>
2025-09-08 10:57:36 +0500

    htaccess updated

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -81,9 +81,6 @@ except ImportError as e:
 # Set the Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bitbio.settings")
 
-# Initialize Django
-django.setup()
-
 # Get the WSGI application
 application = get_wsgi_application()
 
